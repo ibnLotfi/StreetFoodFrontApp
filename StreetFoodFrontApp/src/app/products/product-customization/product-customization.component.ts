35e44f3b30208e08bc951026a63b13feea09607a
ibnLotfi <ibnlotfi@hotmail.fr>
2025-07-22 22:25:17 +0200

    featyre: add initial project structure with product and cart features

---
+++ StreetFoodFrontApp/src/app/products/product-customization/product-customization.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-customization',
+  imports: [],
+  templateUrl: './product-customization.component.html',
+  styleUrl: './product-customization.component.scss'
+})
+export class ProductCustomizationComponent {
+
+}
